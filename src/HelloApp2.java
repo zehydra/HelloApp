4db9e1147877c054da525318f62d4e08b2894f4b
zehydra <prachikumari152007@gmail.com>
2026-03-10 14:28:33 +0530

    use case 3

---
+++ src/HelloApp2.java
@@ -0,0 +1,10 @@
+public class HelloApp2{
+    public static void main(String[] args){
+        String name = "World";
+
+        if(args.length>0){
+            name=args[0];
+        }
+        System.out.println("Hello "+name+" !");
+    }
+}
